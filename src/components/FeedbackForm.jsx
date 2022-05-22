46813bef309a081bfbba766799cbf1088b98cb58
Fotios Zachopoulos <65657774+zachfotis@users.noreply.github.com>
2022-05-23 02:04:44 +0300

    add gh action

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -25,7 +25,7 @@ function FeedbackForm({ handleAdd }) {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (text.trim().length > 10) {
+    if (text.trim().length >= 10) {
       const newFeedback = {
         text,
         rating,
