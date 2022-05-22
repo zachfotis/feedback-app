e4dd73a1b6fc74a5905dd483f284a12f4982cb8f
Fotios Zachopoulos <65657774+zachfotis@users.noreply.github.com>
2022-05-22 19:43:48 +0300

    add feedbackStats comp

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import { useState } from 'react';
+import FeedbackData from './data/FeedbackData';
 import Header from './components/Header';
-import FeedbackItem from './components/FeedbackItem';
+import FeedbackList from './components/FeedbackList';
+import FeedbackStats from './components/FeedbackStats';
 
 function App() {
+  const [feedback, setFeedback] = useState(FeedbackData);
+  const deleteFeedback = (id) => {
+    if (window.confirm('Are you sure you want to delete this feedback?')) {
+      setFeedback(feedback.filter((feedback) => feedback.id !== id));
+    }
+  };
   return (
     <>
       <Header />
       <div className="container">
-        <FeedbackItem />
+        <FeedbackStats feedback={feedback} />
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>
   );
