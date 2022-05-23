1c78a6f264f78a028ff47463bd97d0ca0caebc74
Fotios Zachopoulos <65657774+zachfotis@users.noreply.github.com>
2022-05-23 20:08:30 +0300

    fix routes

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
         <div className="container">
           <Routes>
             <Route
+              exact
               path="/"
               element={
                 <>
